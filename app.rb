97a9cddb4ecb408a12b2c8e89e6f8c8e6e458f51
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-25 19:24:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :new
+  end
+  
+  post '/puppy' do
+    @puppy = Puppy.new(params[:puppy])
+
+    @all_puppy = Puppy.all
+    erb :puppy
+  end
 end
